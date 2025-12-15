c533e4e6fccae382892bd69c4f8f5fa92c81d444
vugarmirzayev <vu9ar.mirzayev@gmail.com>
2025-12-15 15:26:41 +0400

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Text
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram import F
-from aiogram import Router
-from aiogram.utils import executor
+from aiogram import executor
 
 # -------------------- Настройки --------------------
 API_TOKEN = os.getenv("BOT_TOKEN")   # Токен бота
